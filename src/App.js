2279e768eeeab5c137748b5e02508df41c98743b
hikarinakano <hikarinakano95@gmail.com>
2024-09-25 15:57:12 +0200

    Refactored the default index.js and App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Button from "./components/Button";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button primary>Click me!</Button>
+      <Button secondary>Hey!</Button>
+      <Button danger>Call me!</Button>
     </div>
   );
 }
