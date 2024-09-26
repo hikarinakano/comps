05ea436edcceacccec46af205639dedf3a87a3b8
hikarinakano <hikarinakano95@gmail.com>
2024-09-26 15:48:30 +0200

    Added logic to expand window, added icons to show if the window is expanded or closed and added css to show the accordeons elements properly

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,13 +1,36 @@
+import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go"
+
 function Accordion ({ items }) {
-  const renderedItems = items.map(({label, content, id}) => {
+  const [expandedIndex, setExpandedIndex] = useState(-1);
+
+  const handleClick = (nextIndex) => {
+    if (nextIndex === expandedIndex) {
+      setExpandedIndex(-1);
+    }
+    else setExpandedIndex(nextIndex);
+  }
+
+  const renderedItems = items.map(({ id, label, content }, index) => {
+    const isExpanded = index === expandedIndex;
+    const icon = (
+      <span className="text-xl">
+        {isExpanded ? <GoChevronDown/> : <GoChevronLeft/>}
+      </span>
+    );
+
     return (
       <div key={id}>
-        <div>{label}</div>
-        <div>{content}</div>
+        <div className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer" onClick={() => handleClick(index)}>
+          {label}
+          {icon}
+        </div>
+
+        {isExpanded && <div className="border-b p-5">{content}</div>}
       </div>
-    )
-  })
-  return <div>
+    );
+  });
+  return <div className="border-x border-t rounded">
           {renderedItems}
         </div>
 }
