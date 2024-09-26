03b57b7e7f2f01b53c511ad62dacb4d0b8296744
hikarinakano <hikarinakano95@gmail.com>
2024-09-26 13:47:01 +0200

    Added passing of ClassName to the Button component to pass it to the button element

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 
 import Button from "./components/Button";
+import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
 
 function App() {
+
+  const handleClick = () => console.log('Clicked!')
     return (
       <div>
         <div>
-          <Button success rounded outline>
+          <Button success rounded outline className="mb-5" onClick={handleClick} onMouseEnter={handleClick}>
+            <GoBell/>
             Click me!!
           </Button>
         </div>
         <div>
           <Button danger outline>
+            <GoCloudDownload/>
             Buy Now!
           </Button>
         </div>
         <div>
-          <Button warning>See Deal!</Button>
+          <Button warning>
+            <GoDatabase/>
+            See Deal!
+          </Button>
         </div>
         <div>
           <Button secondary outline>
