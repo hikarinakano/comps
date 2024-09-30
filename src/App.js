05da72189255453837d9c26a6f100a3896a9337e
hikarinakano <hikarinakano95@gmail.com>
2024-09-30 16:35:12 +0200

    Added props passed to the Dropdown component and renamed props and function for more clarity

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 
 // import ButtonPage from "./pages/ButtonPage";
 // import AccordionPage from "./components/Accordion";
+import { useState } from "react";
 import Dropdown from "./components/Dropdown"; 
 
 function App() {
+  const [value, setValue] = useState(null);
+
+  const handleSelect = (option) => {
+    setValue(option);
+  }
+  const options = [
+    {label: 'Red', value: 'red'},
+    {label: 'Blue', value: 'blue'},
+    {label: 'Green', value: 'green'},
+  ]
+
   return (
-    <Dropdown />
+    <Dropdown options={options} value={value} onChange={handleSelect} />
   );
 }
 export default App;
