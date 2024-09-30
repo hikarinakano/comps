83bc01caaa5f6f4a6700405e659c2d017ec521ab
hikarinakano <hikarinakano95@gmail.com>
2024-09-30 16:35:33 +0200

    added some tailwind css styling to the component

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,10 +1,30 @@
 import { useState } from "react";
-import className from "classnames";
+// import className from "classnames";
 
-function Dropdown () {
+function Dropdown ({ options, value, onChange }) {
+  
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOptionClick = (option) => {
+    setIsOpen(false);
+    onChange(option)
+  }
+  const handleClick = () => {
+    setIsOpen((currentIsOpen) => !currentIsOpen);
+  }
+  const renderedOptions = options.map((option) => {
+    return (
+      <div className={"hover:bg-sky-100 rounded cursor-pointer p-1"}onClick={() => handleOptionClick(option)} key={option.value}>{option.label}</div>
+    )
+})
+  
+
+
+
   return (
-    <div>
-      Dropdown...
+    <div className="w-48 relative">
+      <div className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full " onClick={handleClick}>{value?.label || 'Select...'}</div>
+      { isOpen && <div className="absolute top-full border rounded p-3 shadow bg-white w-full">{renderedOptions}</div>}
     </div>
   )
 }
