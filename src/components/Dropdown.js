4e4bf7aca03dfa5fdc83e8c86d45acb18733b00f
hikarinakano <hikarinakano95@gmail.com>
2024-09-30 16:57:36 +0200

    Created reusable component Panel.js for the Dropdown styling

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
-// import className from "classnames";
+import { GoChevronDown, GoChevronUp } from "react-icons/go";
+import Panel from "./Panel";
 
 function Dropdown ({ options, value, onChange }) {
   
@@ -23,8 +24,16 @@ function Dropdown ({ options, value, onChange }) {
 
   return (
     <div className="w-48 relative">
-      <div className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full " onClick={handleClick}>{value?.label || 'Select...'}</div>
-      { isOpen && <div className="absolute top-full border rounded p-3 shadow bg-white w-full">{renderedOptions}</div>}
+      <Panel 
+        className="flex justify-between items-center cursor-pointer"
+        onClick={handleClick}>
+          {value?.label || 'Select...'}
+          {isOpen ? <GoChevronUp className="text-lg"/> : <GoChevronDown className="text-lg"/>}
+      </Panel>
+      { isOpen && (
+          <Panel className="absolute top-full">
+            {renderedOptions}
+          </Panel>)}
     </div>
   )
 }
