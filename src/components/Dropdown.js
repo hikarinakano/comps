14839dc7b71b1ef368fc252d26988863c7f0d9da
hikarinakano <hikarinakano95@gmail.com>
2024-09-30 12:17:53 +0200

    Created a new component of Dropdown

---
+++ src/components/Dropdown.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import className from "classnames";
+
+function Dropdown () {
+  return (
+    <div>
+      Dropdown...
+    </div>
+  )
+}
+
+export default Dropdown;
