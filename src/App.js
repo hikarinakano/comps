7de050b178b8ed96bf965396ec898fda94f0e53c
hikarinakano <hikarinakano95@gmail.com>
2024-09-30 12:17:33 +0200

    Created an import of a new component to render in App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 
 // import ButtonPage from "./pages/ButtonPage";
-import Accordion from "./components/Accordion";
+// import AccordionPage from "./components/Accordion";
+import Dropdown from "./components/Dropdown"; 
 
 function App() {
-
-  const items = [
-    {
-      id: '123',
-      label: "Can i use React on a project?",
-      content: 'You can use React on any project you want.You can use React on any project you want.',
-    },
-    {
-      id: '1234',
-      label: 'Can i use Javascript on a project?',
-      content: 'You can use Javascript on any project you want.You can use React on any project you want.You can use React on any project you want.',
-    },
-    {
-      id: '12345',
-      label: 'Can i use CSS a project?',
-      content: 'You can use CSS on any project you want.You can use React on any project you want.You can use React on any project you want.',
-    },
-  ]
-  return (<Accordion items={items}/>)
+  return (
+    <Dropdown />
+  );
 }
 export default App;
