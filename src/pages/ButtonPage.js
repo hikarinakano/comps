4edf0c5a7aed4031f3ca238e019980e96c2fae45
hikarinakano <hikarinakano95@gmail.com>
2024-10-01 16:31:41 +0200

    Wrote the console log in handleClick back

--- src/pages/ButtonPage.js
+++ src/pages/ButtonPage.js
@@ -4,7 +4,7 @@ import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
 
 function ButtonPage() {
 
-  const handleClick = () => console.log('Clicked!')
+  const handleClick = () => console.log('Clicked!');
     return (
       <div>
         <div>
