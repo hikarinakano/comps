97538fd5056950693a216f4919701d3438685287
hikarinakano <hikarinakano95@gmail.com>
2024-10-01 16:17:19 +0200

    Added simple navigation logic to the app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import Link from "./components/Link";
-// import ButtonPage from "./pages/ButtonPage";
-// import AccordionPage from "./components/Accordion";
-// import DropdownPage from "./components/Accordion";
+import Route from "./components/Route";
+import ButtonPage from "./pages/ButtonPage";
+import AccordionPage from "./pages/AccordionPage";
+import DropdownPage from "./pages/DropdownPage";
 
 function App() {
   return (
-    <div>
+    <div className="">
       <Link to={"/accordion"}>Go to Accordion</Link>
       <br></br>
       <Link to={"/dropdown"}>Go to Dropdown</Link>
+      <br></br>
+      <Link to={"/button"}>Go to Button</Link>
+      <br></br>
+      <div className="">
+        <Route path="/accordion">
+          <AccordionPage/>
+        </Route>
+        <Route path="/dropdown">
+          <DropdownPage />
+        </Route>
+        <Route path="/button">
+          <ButtonPage />
+        </Route>
+      </div>
     </div> 
   );
 }
