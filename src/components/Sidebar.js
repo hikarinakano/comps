9e15ca213bd89d58a4ad9cadd6a36afa418c0cec
hikarinakano <hikarinakano95@gmail.com>
2024-10-02 12:18:20 +0200

    Created a Sidebar component that renders all the navigation Link components and adds styling necessary to the component when it's active

---
+++ src/components/Sidebar.js
@@ -0,0 +1,19 @@
+import Link from "./Link";
+
+function Sidebar () {
+  const links = [
+    {label: 'Dropdown', path: '/'},
+    {label: 'Buttons', path: '/buttons'},
+    {label: 'Accordion', path: '/accordion'},
+  ];
+  const renderedLinks = links.map(({label, path}) => {
+    return <Link key={label} to={path} className="mb-3" activeClassName="font-bold border-l-4 border-blue-500 pl-2">{label}</Link>
+  });
+  return (
+    <div className="sticky top-0 overflow-y-scroll flex flex-col items-start">
+      {renderedLinks}
+    </div>
+  );
+}
+
+export default Sidebar;
