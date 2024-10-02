4c6e273302d16612de6af0b55db71facedb0a644
hikarinakano <hikarinakano95@gmail.com>
2024-10-02 12:29:05 +0200

    Added new route for the Modal page to render

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Route from "./components/Route";
 import ButtonPage from "./pages/ButtonPage";
 import AccordionPage from "./pages/AccordionPage";
 import DropdownPage from "./pages/DropdownPage";
+import ModalPage from "./pages/ModalPage";
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
         </Route>
         <Route path="/buttons">
           <ButtonPage />
+        </Route>
+        <Route path="/modal">
+          <ModalPage/>
         </Route>
       </div>
     </div> 
