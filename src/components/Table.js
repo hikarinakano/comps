ba53dae12e1fedbd2bebdcfb00af6cf91753e13b
hikarinakano <hikarinakano95@gmail.com>
2024-10-04 13:27:34 +0200

    Restructured the Table component so it'll look like a real table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,28 @@
 function Table({ data }) {
-  return <div>{data.length}</div>
+  const renderedRows = data.map(({name, color, score}) => {
+    return (
+      <tr className="border-b">
+          <td className="p-3">{name}</td>
+          <td className="p-3">
+            <div className={`p-3 m-2 ${color}`}></div>
+          </td>
+          <td className="p-3">{score}</td>
+        </tr>
+    )
+  })
+  return (
+    <table className="table-auto border-spacing-2">
+      <thead>
+        <tr className="border-b-2">
+          <th>Fruit</th>
+          <th>Color</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>
+        {renderedRows}
+      </tbody>
+    </table>
+  )
 }
-
 export default Table;
