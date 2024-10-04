57db115018727df2217419f890b303e7d62c379b
hikarinakano <hikarinakano95@gmail.com>
2024-10-04 13:28:24 +0200

    Changed some color properties to look nicer

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -3,9 +3,9 @@ import Table from '../components/Table'
 function TablePage() {
   const data = [
     {name: "Orange", color:"bg-orange-500", score: 5},
-    {name: "Apple", color:"bg-red-300", score: 3},
-    {name: "Banana", color:"bg-yellow-500", score: 1},
-    {name: "Lime", color:"bg-green-500", score: 4},
+    {name: "Apple", color:"bg-red-400", score: 3},
+    {name: "Banana", color:"bg-yellow-400", score: 1},
+    {name: "Lime", color:"bg-green-400", score: 4},
   ];
   return (
     <div>
