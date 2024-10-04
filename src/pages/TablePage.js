6a402eb367e8c293bb8fe93096a71287cae71db6
hikarinakano <hikarinakano95@gmail.com>
2024-10-04 15:02:43 +0200

    Refactored the config list and added a key function to pass to Table component

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -2,14 +2,33 @@ import Table from '../components/Table'
 
 function TablePage() {
   const data = [
-    {name: "Orange", color:"bg-orange-500", score: 5},
-    {name: "Apple", color:"bg-red-400", score: 3},
-    {name: "Banana", color:"bg-yellow-400", score: 1},
-    {name: "Lime", color:"bg-green-400", score: 4},
+    {name: "Orange", color: "bg-orange-500", score: 5},
+    {name: "Apple", color: "bg-red-400", score: 3},
+    {name: "Banana", color: "bg-yellow-400", score: 1},
+    {name: "Lime", color: "bg-green-400", score: 4},
   ];
+
+  const config = [
+    {
+      label: "Name",
+      render: (fruit) => fruit.name,
+    },
+    {
+      label: "Color",
+      render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
+    },
+    {
+      label: "Score",
+      render: (fruit) => fruit.score,
+    },
+  ]
+
+  const keyFn = (fruit) => {
+    return fruit.name
+  }
   return (
     <div>
-      <Table data={data}></Table>
+      <Table data={data} config={config} keyFn={keyFn}></Table>
     </div> 
   );
 }
