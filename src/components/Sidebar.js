b77485ac5db71c466a49bcbc2a0b1aa09fca37b4
hikarinakano <hikarinakano95@gmail.com>
2024-10-07 12:06:51 +0200

    Set up a new route and navigation to a new Counter page

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,6 +7,7 @@ function Sidebar () {
     {label: 'Accordion', path: '/accordion'},
     {label: 'Modal', path: '/modal'},
     {label: 'Table', path: '/table'},
+    {label: 'Counter', path:'/counter'},
   ];
   const renderedLinks = links.map(({label, path}) => {
     return <Link key={label} to={path} className="mb-3" activeClassName="font-bold border-l-4 border-blue-500 pl-2">{label}</Link>
