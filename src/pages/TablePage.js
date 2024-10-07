1d5d1a832574ec4eb5eb8c17f2de3c08ab384236
hikarinakano <hikarinakano95@gmail.com>
2024-10-07 10:21:49 +0200

    Adding special sortValue for the columns that supposed to be sortable

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,4 +1,5 @@
-import Table from '../components/Table'
+// import Table from '../components/Table';
+import SortableTable from '../components/SortableTable';
 
 function TablePage() {
   const data = [
@@ -12,24 +13,26 @@ function TablePage() {
     {
       label: "Name",
       render: (fruit) => fruit.name,
+      sortValue: (fruit) => fruit.name,
     },
     {
       label: "Color",
       render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
     },
     {
-      header: () => <th className="bg-red-500">Score</th>,
       label: "Score",
       render: (fruit) => fruit.score,
+      sortValue: (fruit) => fruit.score,
     },
   ]
 
   const keyFn = (fruit) => {
-    return fruit.name
+    return fruit.name;
   }
+
   return (
     <div>
-      <Table data={data} config={config} keyFn={keyFn}></Table>
+      <SortableTable data={data} config={config} keyFn={keyFn}></SortableTable>
     </div> 
   );
 }
